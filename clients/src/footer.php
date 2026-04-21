0c0735f2c3bc26221629ac1735bbe6e2f49a4501
jan16hcm <tranhoangkhai271625@gmail.com>
2026-04-21 11:15:16 +0700

    Add footer for pages

--- clients/src/footer.php
+++ clients/src/footer.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <footer class="text-center mt-5">
-        <p>&copy; 2026 E-Wallet Project - KhaiDev</p>
+        <p class="fs-5">&copy; 2026 Meo Meo Wallet Final Project</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
