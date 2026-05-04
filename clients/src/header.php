538a3e258e78497ee019a3e678a27794febed053
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-04 08:57:36 +0700

    refactor: click header to redirect page

--- clients/src/header.php
+++ clients/src/header.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <title>E-Wallet System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css">
-    
+
 </head>
+
 <body>
-    <header class="sticky-top"> 
-        <div class="container-fluid bg-primary py-2 text-white ps-4">
-            <span class="fw-bold fs-4">MeoMeo Wallet</span>
+    <header class="sticky-top">
+        <!-- <div class="container-fluid bg-primary py-2 text-white ps-4">
+            <button type="button" class="btn" onclick="location.href='../pages/Home.php'">
+                <span class="fw-bold fs-4 text-white">MeoMeo Wallet</span></button>
+        </div> -->
+        <div class="container-fluid bg-primary py-2 text-white ps-4 d-flex align-items-center">
+            <a href="../pages/Home.php" class="text-decoration-none">
+                <span class="fw-bold fs-4 text-white">MeoMeo Wallet</span>
+            </a>
         </div>
     </header>
     <!-- <nav class="navbar navbar-dark bg-primary py-1 position-sticky top-0"> 
@@ -19,4 +27,5 @@
         </div>
     </nav> -->
 </body>
+
 </html>
