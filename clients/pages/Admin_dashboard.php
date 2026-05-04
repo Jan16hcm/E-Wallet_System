3fa0e9c00067ddbfeb1ec9e40f9069d0a40a3663
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-04 09:59:20 +0700

    minor: verified admin

--- clients/pages/Admin_dashboard.php
+++ clients/pages/Admin_dashboard.php
@@ -1,3 +1,9 @@
-<?php
-    echo"Admin page";
+<?php 
+    session_start();
+    if($_SESSION['verified'] != 3) {
+        echo "khong phai admin khong cho truy cap";
+        
+    } else {
+        echo"Admin page";
+    }
 ?>
