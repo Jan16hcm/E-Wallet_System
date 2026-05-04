105a29850a65fd7a3c832eec6b3288127a665e0d
mnmnmnmnm001 <252169338+mnmnmnmnm001@users.noreply.github.com>
2026-05-04 11:14:34 +0700

    Initialize session handling in db_connection.php
    Added session_start() to initiate session handling.

--- clients/modules/db_connection.php
+++ clients/modules/db_connection.php
@@ -1,4 +1,7 @@
 <?php 
+    session_start();
+    //include this file at the start of every php
+    // require_once("../modules/db_connection.php");
     function connect_db(){
         $con = new mysqli("localhost", "root", "", "fakebank");
         if ($con->connect_error) {
