2b251ce9f56713c0d69e1c9ed3cc6023442fc6a2
mnmnmnmnm001 <252169338+mnmnmnmnm001@users.noreply.github.com>
2026-05-04 09:36:27 +0700

    connect_db.php
    Refactored database connection code into a function.

--- clients/modules/db_connection.php
+++ clients/modules/db_connection.php
@@ -1,13 +1,10 @@
 <?php 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "fakebank";
-
-    $conn = new mysqli($host, $username, $password, $dbname);
-
-    if($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    function connect_db(){
+        $con = new mysqli("localhost", "root", "", "fakebank");
+        if ($con->connect_error) {
+            die("Connection failed: " . $con->connect_error);
+        }
+        $con->set_charset("utf8mb4");
+        return $con;
     }
-    $conn->set_charset("utf8mb4");
 ?>
