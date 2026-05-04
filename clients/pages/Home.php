fe4b3973ad83f5e45adf229307a3fe4867cb6922
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-04 11:02:00 +0700

    fix utf8 for name

--- clients/pages/Home.php
+++ clients/pages/Home.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
-    if (isset($_SESSION['user_phone']) || isset($_SESSION['user_name'])) {
+    if (!isset($_SESSION['user_phone']) || !isset($_SESSION['user_name'])) {
         header("Location: Login.php");
         exit();
     }
     include '../src/header.php';
 
-    $username = $_SESSION['user_name'];
-    $userphone = $_SESSION['user_phone'];
+    $username = htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8');
+    $userphone = htmlspecialchars($_SESSION['user_phone'], ENT_QUOTES, 'UTF-8');
     echo "<h1 class='text-center mt-5'>Welcome, $username!</h>";
     echo "<h2 class='text-center mb-4'>Your phone number: $userphone</h2>";
 
