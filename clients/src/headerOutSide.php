eb3ad87846a46ec92d4d241735debcc2d5271531
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-04 09:57:38 +0700

    add: header for login, signup, forgot

---
+++ clients/src/headerOutSide.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>E-Wallet System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+</head>
+
+<body>
+    <header class="sticky-top">
+        <!-- <div class="container-fluid bg-primary py-2 text-white ps-4">
+            <button type="button" class="btn" onclick="location.href='../pages/Home.php'">
+                <span class="fw-bold fs-4 text-white">MeoMeo Wallet</span></button>
+        </div> -->
+        <div class="container-fluid bg-primary py-2 text-white ps-4 d-flex align-items-center">
+            <a href="../pages/Home.php" class="text-decoration-none">
+                <span class="fw-bold fs-4 text-white">MeoMeo Wallet</span>
+            </a>
+        </div>
+    </header>
+    <!-- <nav class="navbar navbar-dark bg-primary py-1 position-sticky top-0"> 
+        <div class="container ms-4"> 
+            <a class="navbar-brand m-0 fw-bold fs-4">MeoMeo Wallet</a>
+        </div>
+    </nav> -->
+</body>
+
+</html>
