bb04d297ea068ecb8b70b8fe917b4fd0d7f82e9c
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-04 09:58:37 +0700

    minor: show infor user

--- clients/pages/Home.php
+++ clients/pages/Home.php
@@ -1,3 +1,15 @@
 <?php
-    echo"User page";
+    session_start();
+    if (!isset($_SESSION['user_phone']) || !isset($_SESSION['user_name'])) {
+        header("Location: Login.php");
+        exit();
+    }
+    include '../src/header.php';
+
+    $username = $_SESSION['user_name'];
+    $userphone = $_SESSION['user_phone'];
+    echo "<h1 class='text-center mt-5'>Welcome, $username!</h>";
+    echo "<h2 class='text-center mb-4'>Your phone number: $userphone</h2>";
+
+    include '../src/footer.php';
 ?>
