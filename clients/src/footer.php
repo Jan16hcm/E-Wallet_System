0c83e7785551e66b13721da2b63cc6b943336516
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-06 02:18:50 +0700

    refactor: header and footer for page DOM

--- clients/src/footer.php
+++ clients/src/footer.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>E-Wallet System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
-
-<body>
-    <!-- <footer class="text-center mt-3 bottom-0 py-2">
-        <p class="fs-6 mb-0">&copy; 2026 MeoMeo Wallet Final Project</p>
-    </footer> -->
-    <!-- <footer class="border-top footer text-muted">
-        <div class="container text-center">
-            &copy; 2026 MeoMeo Wallet Final Project
-        </div>
-    </footer> -->
-    <footer class="border-top footer text-muted bottom bg-white">
+<footer class="border-top footer text-muted bottom bg-white mt-auto">
         <div class="container text-center py-3">
             &copy; 2026 MeoMeo Wallet Final Project
         </div>
     </footer>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/js/main.js"></script>
 </body>
-
 </html>
