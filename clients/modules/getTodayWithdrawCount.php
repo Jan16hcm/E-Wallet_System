4351d9292a2455c51b906edd86306c39fb901532
mnmnmnmnm001 <252169338+mnmnmnmnm001@users.noreply.github.com>
2026-05-06 21:27:47 +0700

    fix database connect in getTodayWithdrawCount.php

--- clients/modules/getTodayWithdrawCount.php
+++ clients/modules/getTodayWithdrawCount.php
@@ -6,7 +6,6 @@
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $user_phone = '';
-        $stmt->get_result();
         $stmt->bind_result($user_phone);
         $stmt->fetch();//done get user phonenum
         
@@ -18,16 +17,16 @@
         }
         */
 
-        $stmt = $con->prepare("SELECT COUNT(*) FROM history WHERE user_phone = ? AND transfer_type='Withdraw' AND DATE(date_transfer) = CURDATE() AND status != 0");
+        $stmt = $con->prepare('SELECT COUNT(*) FROM history WHERE user_phone = ? AND transfer_type = Withdraw AND DATE(date_transfer) = CURDATE() AND status != 0');
         //DATE(a) removes the time part and keeps only YYYY-MM-D
         //CURDATE() function in MySQL returns the current date in the format YYYY-MM-DD (string) 
         $stmt->bind_param("s", $user_phone);
         $stmt->execute();
         $count = 0;
-        $stmt->get_result();
         $stmt->bind_result($count);
         $stmt->fetch();
         $stmt->close();
+        $con->close();
         return (int)$count;
     }
 ?>
