6038d8f1a22ad0a46fb2be1d0c356ff5f6917ca1
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-07 16:00:40 +0700

    dump database form server

--- database/database.sql
+++ database/database.sql
@@ -1,107 +1,104 @@
-﻿-- phpMyAdmin SQL Dump
--- version 5.2.3
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost
--- Generation Time: Apr 14, 2026 at 03:31 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: fakebank
+-- ------------------------------------------------------
+-- Server version	8.0.45-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: fakebank
+-- Table structure for table `history`
 --
-CREATE DATABASE IF NOT EXISTS fakebank DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE fakebank;
-
--- --------------------------------------------------------
 
 DROP TABLE IF EXISTS `history`;
-DROP TABLE IF EXISTS `user`;
---
--- Table structure for table user
---
-
-CREATE TABLE `user` (
-  `phonenum` varchar(15) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `birth` date DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `front` mediumblob DEFAULT NULL,
-  `back` mediumblob DEFAULT NULL,
-  `pass` varchar(255) NOT NULL,
-  `verified` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0:False\n1:True\n2:Request additional information\n3:IsAdmin True\n4:DisableAccount',
-  `abnormal_login` tinyint(4) NOT NULL DEFAULT 0 COMMENT '>3 lock account',
-  `card_num` varchar(6) DEFAULT NULL,
-  `money` decimal(19,4) NOT NULL DEFAULT 0.0000,
-  `CVV` varchar(3) DEFAULT NULL,
-  `expiration` date DEFAULT NULL,
-  `locked_time` DATETIME DEFAULT NULL COMMENT 'Disable account overtime',
-  PRIMARY KEY (`phonenum`) -- SET PRIMARYKEY
-  UNIQUE KEY (`email`) -- SET UNIQUE KEY
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
---
--- Table structure for table history
---
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT, -- Auto increase id
-  `user_phone` varchar(15) NOT NULL COMMENT 'SĐT người thực hiện giao dịch',
-  `receiver_phone` varchar(15) DEFAULT NULL COMMENT 'SĐT người nhận (nếu là chuyển khoản)',
-  `transfer_type` varchar(50) DEFAULT 'Deposit' COMMENT 'Deposit/Transferto/Transferby/Withdraw/Buycard',
-  `card_num` varchar(6) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'SĐT người thực hiện giao dịch',
+  `receiver_phone` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'SĐT người nhận (nếu là chuyển khoản)',
+  `transfer_type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT 'Transferto' COMMENT 'Transferto/Transferby/Withdraw/Buycard',
+  `card_num` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
   `expiration` date NOT NULL,
-  `CVV` varchar(3) NOT NULL,
+  `CVV` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_transfer` datetime NOT NULL,
   `date_confirm` datetime DEFAULT NULL COMMENT 'date admin approve/cancelled',
-  `money` decimal(19,4) NOT NULL DEFAULT 0.0000,
-  `fee` decimal(19,4) NOT NULL DEFAULT 0.0000,
-  `fee_bearer` tinyint(1)    DEFAULT NULL,
-  `note` mediumtext DEFAULT NULL,
-  `phone_card` JSON DEFAULT NULL COMMENT 'an array of phone card, max 5, replace nvarchar(255) to JSON',
-  `status` tinyint(4) NOT NULL DEFAULT 2 COMMENT '0:Cancelled\n1:Approved\n2:Pending',
-  PRIMARY KEY (`id`), -- SET key
+  `money` decimal(19,4) NOT NULL DEFAULT '0.0000',
+  `note` mediumtext COLLATE utf8mb4_unicode_ci,
+  `phone_card` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'an array of phone card, max 5',
+  `status` tinyint NOT NULL DEFAULT '2' COMMENT '0:Cancelled\n1:Approved\n2:Pending',
+  PRIMARY KEY (`id`),
+  KEY `fk_history_user` (`user_phone`),
   CONSTRAINT `fk_history_user` FOREIGN KEY (`user_phone`) REFERENCES `user` (`phonenum`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table user
+-- Dumping data for table `history`
 --
 
--- 1. Admin
-INSERT INTO `user` (`phonenum`, `email`, `name`, `birth`, `address`, `pass`, `verified`, `money`) 
-VALUES 
-('0000000000', 'admin@meomeo.com', 'Admin', '2005-01-16', 'TDTU', '$2y$10$B6hq495fsonInn5zlBf7mOn.CbplMG7/Y87wCD1go.chQeGWdNk8y', 3, 0.0000);
+LOCK TABLES `history` WRITE;
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- 2. User already verified (verified = 1) - Have 5 million
-INSERT INTO `user` (`phonenum`, `email`, `name`, `birth`, `address`, `pass`, `verified`, `money`) 
-VALUES 
-('0912345678', 'user1@gmail.com', 'Tran Hoang Khai', '1995-05-15', 'Hồ Chí Minh', '$2y$10$Wyq/jXyw6jwyxjjvxy1.ceYiYODYEOS34GfitOqthWL1llDPmZlBe', 1, 5000000.0000);
+--
+-- Table structure for table `user`
+--
 
--- 3. Account User wait for verified (verified = 0) - Wait and no money
-INSERT INTO `user` (`phonenum`, `email`, `name`, `birth`, `address`, `pass`, `verified`, `money`) 
-VALUES 
-('0987654321', 'user2@gmail.com', 'Tran Thi B', '2000-10-20', 'Hà Nội', '$2y$10$WdH8txReFwer4lGWxptvpuW6D0L7MdFDTqwhGoWgNmY8m9daATH.K', 0, 0.0000);
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `phonenum` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birth` date DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `front` mediumblob,
+  `back` mediumblob,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0:False\n1:True\n2:Request additional information\n3:IsAdmin True\n4:DisableAccount',
+  `abnormal_login` tinyint NOT NULL DEFAULT '0' COMMENT '>3 lock account',
+  `card_num` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `money` decimal(19,4) NOT NULL DEFAULT '0.0000',
+  `CVV` varchar(3) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `expiration` date DEFAULT NULL,
+  `locked_time` datetime DEFAULT NULL COMMENT 'Disable account overtime',
+  PRIMARY KEY (`phonenum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 4. Account User disable (verified = 4)
-INSERT INTO `user` (`phonenum`, `email`, `name`, `birth`, `address`, `pass`, `verified`, `money`) 
-VALUES 
-('0909090909', 'user3@gmail.com', 'Le Van C', '1998-02-28', 'Đà Nẵng', '$2y$10$BBstCMneuLr2RJoEgEtOUualLHCJtTrC3HVvIPwOg/vYTXwxgATsO', 4, 150000.0000);
+--
+-- Dumping data for table `user`
+--
 
-COMMIT;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('0000000000','admin@meomeo.com','Admin','2005-01-16','TDTU',NULL,NULL,'$2y$10$B6hq495fsonInn5zlBf7mOn.CbplMG7/Y87wCD1go.chQeGWdNk8y',3,0,NULL,0.0000,NULL,NULL,NULL),('0909090909','user3@gmail.com','Le Van C','1998-02-28','Đà Nẵng',NULL,NULL,'$2y$10$BBstCMneuLr2RJoEgEtOUualLHCJtTrC3HVvIPwOg/vYTXwxgATsO',4,0,NULL,150000.0000,NULL,NULL,NULL),('0912345678','user1@gmail.com','Tran Hoang Khai','1995-05-15','Hồ Chí Minh',NULL,NULL,'$2y$10$Wyq/jXyw6jwyxjjvxy1.ceYiYODYEOS34GfitOqthWL1llDPmZlBe',1,0,NULL,5000000.0000,NULL,NULL,NULL),('0987654321','user2@gmail.com','Tran Thi B','2000-10-20','Hà Nội',NULL,NULL,'$2y$10$WdH8txReFwer4lGWxptvpuW6D0L7MdFDTqwhGoWgNmY8m9daATH.K',0,0,NULL,0.0000,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping routines for database 'fakebank'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-07 15:56:09
