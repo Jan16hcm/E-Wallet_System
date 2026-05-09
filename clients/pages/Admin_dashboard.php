135057cb22bcfbe5703f03c98bea84b3495a7f9a
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-05-10 00:35:49 +0700

    docx: reformat js

--- clients/pages/Admin_dashboard.php
+++ clients/pages/Admin_dashboard.php
@@ -366,35 +366,7 @@ include '../src/header.php';
     </a>
 </div>
 
-<script>
-    function switchTab(tabId) {
-        document.querySelectorAll('.admin-tab').forEach(t => t.classList.remove('active'));
-        document.querySelectorAll('.admin-list').forEach(l => l.classList.remove('active'));
-        
-        event.target.classList.add('active');
-        document.getElementById('list-' + tabId).classList.add('active');
-    }
-
-    const themeToggleBtns = document.querySelectorAll('.theme-toggle');
-    const body = document.body;
-
-    themeToggleBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            body.classList.toggle('light-theme');
-            themeToggleBtns.forEach(b => {
-                const icon = b.querySelector('i');
-                if (icon) {
-                    if (body.classList.contains('light-theme')) {
-                        icon.classList.replace('fa-moon', 'fa-sun');
-                    } else {
-                        icon.classList.replace('fa-sun', 'fa-moon');
-                    }
-                }
-            });
-        });
-    });
-</script>
+<script src="../assets/js/admin.js"></script>
 
 <?php
     include '../src/footer.php';
